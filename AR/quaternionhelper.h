8c1065ecf8a7cbbdeff92f4283dd0e08ce0c8b10
lorenzolightsndwarf <lor.lucignano@gmail.com>
2016-04-15 19:30:50 +0200

    Add rotateVector in QuaternionHelper

--- AR/quaternionhelper.h
+++ AR/quaternionhelper.h
@@ -2,13 +2,13 @@
 #define QUATERNIONHELPER_H
 
 #include <QObject>
-
+#include <QQuaternion>
 class QuaternionHelper : public QObject
 {
     Q_OBJECT
 public:
     explicit QuaternionHelper(QObject *parent = 0);
-
+    Q_INVOKABLE QVector3D rotateVector(QQuaternion q,QVector3D v);
 signals:
 
 public slots:
